b93311cfae5f654a846aeea0a6bb954af5c57a86
ryanarine <47193209+ryanarine@users.noreply.github.com>
2019-11-12 12:44:32 -0500

    Updates

---
+++ precache-manifest.dd66ac63b7e8aedab80a4ca8fa8774ae.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0e493baea2ad2bfff3aa40d121d62b6f",
+    "url": "/Hangman/index.html"
+  },
+  {
+    "revision": "8a8eae36f814a5cabf1e",
+    "url": "/Hangman/static/css/main.10bfc134.chunk.css"
+  },
+  {
+    "revision": "8575bcc4ab0c58846407",
+    "url": "/Hangman/static/js/2.c3b64fc1.chunk.js"
+  },
+  {
+    "revision": "8a8eae36f814a5cabf1e",
+    "url": "/Hangman/static/js/main.f9e9eb99.chunk.js"
+  },
+  {
+    "revision": "babc746456d36efddaf6",
+    "url": "/Hangman/static/js/runtime-main.920e130f.js"
+  },
+  {
+    "revision": "40a436ab3328741f47147faf0d14142f",
+    "url": "/Hangman/static/media/hangman.40a436ab.svg"
+  }
+]);
